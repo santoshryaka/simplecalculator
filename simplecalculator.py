5d889aed66080555fe3b7b7bfc0c77eb526c95a7
Santosh Ryaka <ryakasantosh@gmail.com>
2026-05-06 19:50:56 +0530

    Update simplecalculator.py

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,1 +1,22 @@
+print("_________Simple Calculator__________")
 
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+#choose the operator 
+print("Choose operation: +, -, *, /")
+op = input("Enter operator: ")
+
+if op == '+':
+    print("Result:", a + b)
+elif op == '-':
+    print("Result:", a - b)
+elif op == '*':
+    print("Result:", a * b)
+elif op == '/':
+    if b != 0:
+        print("Result:", a / b)
+    else:
+        print("Cannot divide by zero")
+else:
+    print("Invalid operator")
