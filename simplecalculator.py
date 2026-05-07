6168cacf8569bb6d4217b6629530459cf7eb048e
Santosh Ryaka <ryakasantosh@gmail.com>
2026-05-07 20:36:21 +0530

    Update simplecalculator.py

--- simplecalculator.py
+++ simplecalculator.py
@@ -6,22 +6,22 @@ a = float(input("Enter first number:"))
 b = float(input("Enter second number:"))
 
 #Choose the operator 
-print("Choose operation: +, -, *, /")
+print("Choose operation(+, -, *, /):")
 op = input("Enter operator:")
 
 #Here the condition will start based on operator
 if op == '+':
-    print("Result:", a + b)
+    print("Result:",a + b)
 elif op == '-':
-    print("Result:", a - b)
+    print("Result:",a - b)
 elif op == '*':
-    print("Result:", a * b)
+    print("Result:",a * b)
 elif op == '/':
     if b != 0:
-        print("Result:", a / b)
+        print("Result:",a / b)
     else:
         print("Cannot divide by zero")
 else:
     print("Invalid operator")
     
-print("_______________Thank you______________")
+print("____________Thank you____________")
